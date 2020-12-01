c2341e0bdb0fc94eb384c7e3f750d3b85ab892ce
pan <pan.wei@topcheer.com>
2020-12-01 14:05:00 +0800

    跳转DisplayMessageActivity

---
+++ AppJAVA/app/src/main/java/com/example/appjava/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package com.example.appjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
--- AppJAVA/app/src/main/java/com/example/appjava/MainActivity.java
+++ AppJAVA/app/src/main/java/com/example/appjava/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.appjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+//    1201-2
+    public static final String EXTRA_MESSAGE = "com.example.appjava.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    1201-1 Called when the user taps the Send button
+//    您可能会看到一条错误，因为 Android Studio 无法解析用作方法参数的 View 类。
+//    要清除错误，请点击 View 声明，将光标置于其上，然后按 Alt+Enter（或在 Mac 上按 Option+Enter）进行快速修复。
+//    如果出现一个菜单，请选择 Import class。
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+//        将启动一个由 Intent 指定的 DisplayMessageActivity 实例
+        startActivity(intent);
+    }
+
 }
