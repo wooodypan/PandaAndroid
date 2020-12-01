3e5f8ef486893ff49d4652a2ff5495c9b350367f
pan <pan.wei@topcheer.com>
2020-12-01 14:25:27 +0800

    starting-activity

--- AppJAVA/app/src/main/java/com/example/appjava/DisplayMessageActivity.java
+++ AppJAVA/app/src/main/java/com/example/appjava/DisplayMessageActivity.java
@@ -1,7 +1,10 @@
 package com.example.appjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        // 获取启动此activity的Intent并提取字符串
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //捕获TextView的布局并将字符串设置为其文本
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
