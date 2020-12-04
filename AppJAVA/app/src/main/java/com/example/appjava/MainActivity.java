be5ae8c74e32298ef10fb3cdd7b2c82e5715ed00
Panda <Panda@gmail.com>
2020-12-04 14:07:14 +0800

    主题
    LOG和Toast

--- AppJAVA/app/src/main/java/com/example/appjava/MainActivity.java
+++ AppJAVA/app/src/main/java/com/example/appjava/MainActivity.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //    1201-2
     public static final String EXTRA_MESSAGE = "com.example.appjava.MESSAGE";
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
     }
 
 //    1201-1 Called when the user taps the Send button
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 //    如果出现一个菜单，请选择 Import class。
     public void sendMessage(View view) {
         // Do something in response to button
+        Log.i(TAG,"hello panda");
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
@@ -30,5 +40,9 @@ public class MainActivity extends AppCompatActivity {
 //        将启动一个由 Intent 指定的 DisplayMessageActivity 实例
         startActivity(intent);
     }
-
+//    弹出Toast
+    public void toastMessage(String msg) {
+        Toast.makeText(MainActivity.this, msg,
+                Toast.LENGTH_SHORT).show();
+    }
 }
